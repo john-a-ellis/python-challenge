297b9c7fd9d8b8a2e6bee5119a1970ea1367d427
john-a-ellis <145600714+john-a-ellis@users.noreply.github.com>
2023-11-09 17:04:56 -0500

    with updated comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,5 @@
+#Election Results Analyis.  Must be run from the PyPoll Folder.
+
 # Modules
 import os
 import csv
@@ -6,45 +8,46 @@ import csv
 
 # Set CSV path for file to the Resource Folder
 csvpath = os.path.join(".", "Resources", "election_data.csv")
-# Set the output path to the Analysis Folder
-# outpath = os.path.join(".", "analysis", "election_analysis.txt")
+#Create two lists one list of integers to store Candidate Vote Counts and one list of strings of Candidate Names
+intCandidateVotes = []
+strCandidates = []
 
 
 with open(csvpath, encoding='UTF-8') as csvfile:
-    intCandidateVotes = []
-    strCandidates = []
 
     csvreader = csv.reader(csvfile, delimiter=",")
     csv_header = next(csvreader)
+    #Read the first header row.
     listheader = csv_header
-    # print(listheader)
-
+    #read through the rest of the file by row.
     for myRow in csvreader:
-
+        #Check to see if the Candidate name appearing on the row can be found in the list of Candidates. 
+        #If the Candidate isn't found in the current list append them to it and add an entry in the Votes list
+        #counting one vote.
         if myRow[2] not in strCandidates:
             strCandidates.append(myRow[2])
             intCandidateVotes.append(1)
         else:
-            foundCandidate=myRow[2]
-            # print(foundCandidate)
-            
-            iVote = strCandidates.index(foundCandidate)
-            cVotes=intCandidateVotes[iVote]+1
-           
-            intCandidateVotes[iVote] = cVotes
+            # if the Candidate is found in the Candidate list add the vote to the corresponding vote tally in the
+            # Candidates vote count.
+            foundCandidate=myRow[2]          
+            iVote = strCandidates.index(foundCandidate) #find the index for the candidate
+            cVotes=intCandidateVotes[iVote]+1 #add the vote to the candidates tally
+            intCandidateVotes[iVote] = cVotes #return the updated canadidates tally to the list.
     
-    iWinner = max(intCandidateVotes)
+    iWinner = max(intCandidateVotes) #once file is processed find the index of the candidate with the most votes
     
-    sWinner = strCandidates[intCandidateVotes.index(iWinner)]
+    sWinner = strCandidates[intCandidateVotes.index(iWinner)] #use that index to identify the winner
     
-    totalvotes=0
+    totalvotes=0 #create a storate for the total votes.
+    #lets sum up the voltes for all candidates to determine the total voted.
     for Candidate in range(len(strCandidates)):
         totalvotes += intCandidateVotes[Candidate]
     print ("Election Results \n")
     print ("-------------------------------------\n")
     print (f"Total Votes: {totalvotes}\n")
     print ("-------------------------------------\n")
-
+    #Print out the results for each candidate and make sure they're formatted correctly.
     for Candidate in range(len(strCandidates)):
         print(strCandidates[Candidate] + ": {0:.3%} ({1})\n".format(intCandidateVotes[Candidate]/totalvotes, intCandidateVotes[Candidate]) )
         
@@ -52,7 +55,7 @@ with open(csvpath, encoding='UTF-8') as csvfile:
     print(f"Winner: {sWinner}\n")
     print("--------------------------------------\n")
 
-#Write it out again to the Text File
+#Write it out again to the Text File 
 txtfile = open(".\\analysis\election_analysis.txt","w")
 txtfile.write("Election Results \n")
 txtfile.write("-------------------------------------\n")
